54249fa7efd15e0653d60274b215e2d1a896d6b4
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-31 15:47:45 -0400

    Add cat sound variable and update console log for pet cat

--- DeclaringVariables/variables.js
+++ DeclaringVariables/variables.js
@@ -1,5 +1,7 @@
 var petDog = "Rex";
 var petCat = "Pepper";
 
-console.log(petCat);
+var catSound = "meow";
+
+console.log(petCat + " says " + catSound);
 console.log("My pet dog's name is " + petDog +".");
