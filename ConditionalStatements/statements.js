36c061535b160ef34129f72cd9210bc1616faa35
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-01 15:42:33 -0400

    Add conditional statements for medal ranking in statements.js

---
+++ ConditionalStatements/statements.js
@@ -0,0 +1,29 @@
+for (let i = 1; i <= 10; i++)
+{
+    if (i === 1)
+    {
+        console.log("Gold Medal");
+    }
+    else if (i === 2)
+    {
+        console.log("Silver Medal");
+    }
+    else if (i === 3) {
+        console.log("Bronze Medal");
+    }
+    else
+        console.log(i);
+}
+
+for (let i = 1; i <= 10; i++)
+{
+    switch (i)
+    {
+        case 1: console.log("Gold Medal"); break;
+        case 2: console.log("Silver Medal"); break;
+        case 3: console.log("Bronze Medal"); break;
+
+    }
+
+        
+}
