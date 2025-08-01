7f78aa37821718e034fe04a2c39b403fd6740a6f
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-01 10:50:50 -0400

    Fix formatting and add missing newlines in variables.js

--- DeclaringVariables/variables.js
+++ DeclaringVariables/variables.js
@@ -3,6 +3,13 @@ var petCat = "Pepper";
 
 var catSound = "meow";
 
-
 console.log(petCat + " says " + catSound);
-console.log("My pet dog's name is " + petDog +".");
+console.log("My pet dog's name is " + petDog + ".");
+
+var score = 8;
+var midSkillBool = score > 0 && score < 10;
+console.log("Mid-level skills: " + midSkillBool);
+
+var timeRemaining = 0;
+var energy = 10;
+console.log("Game Over: " + (timeRemaining == 0 || energy == 0));
