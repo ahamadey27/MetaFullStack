ff20635dac4f4671e0d7d38b96f6e0252f7e6905
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-01 13:28:33 -0400

    Add save button and implement save function in index.js

--- JSCodeCamp/PassangerCounter/index.js
+++ JSCodeCamp/PassangerCounter/index.js
@@ -2,10 +2,8 @@
 // Get the element that displays the count
 let countEl = document.getElementById("count-el");  
 console.log(countEl);
-
 // Initialize the count variable
 let count = 0;
-
 // Function to increment the count and update the display
 function increment()
 {
@@ -13,5 +11,11 @@ function increment()
     countEl.innerText = count;
     console.log(count);
 }
+
+function save()
+{
+    console.log(count);
+}
+ 
 
 
