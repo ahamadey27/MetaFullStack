0a89b1c8937a54f5e5ed5e95cd22b6bda1ef612f
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-01 13:14:36 -0400

    Add INCREMENT button to index.html and implement increment function in index.js

--- JSCodeCamp/PassangerCounter/index.js
+++ JSCodeCamp/PassangerCounter/index.js
@@ -1,4 +1,22 @@
 //document.getElementById("count-el").innerText = 5
 
+// let count = 0;
+// console.log(count);
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+let countEl = document.getElementById("count-el");  
+console.log(countEl);
 let count = 0;
-console.log(count);
+function increment()
+{
+    count++;
+    countEl.innerText = count;
+    console.log(count);
+
+}
+
+
