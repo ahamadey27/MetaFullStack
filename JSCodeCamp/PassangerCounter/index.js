7a1875ecb4b03ca97b97c5678e01240b3a72d162
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-01 13:15:39 -0400

    Remove commented-out code and clean up increment logic in index.js

--- JSCodeCamp/PassangerCounter/index.js
+++ JSCodeCamp/PassangerCounter/index.js
@@ -1,13 +1,5 @@
-//document.getElementById("count-el").innerText = 5
 
-// let count = 0;
-// console.log(count);
-
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
-
+//Logic to create increment increase button
 let countEl = document.getElementById("count-el");  
 console.log(countEl);
 let count = 0;
