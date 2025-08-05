24f0a5cb7020401e22740b22056d621f19fde931
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-05 17:50:04 -0400

    Add initial setup for cloths array and favCar object in index.js

---
+++ CreateArraysAndObjects/index.js
@@ -0,0 +1,15 @@
+let cloths = [];
+cloths.push("shoes");
+cloths.push("shirts");
+cloths.push("pants");
+cloths.push("socks");
+cloths.push("jeans");
+
+cloths.pop();
+console.log(cloths[2])
+
+let favCar = {};
+favCar.color = "green";
+favCar.convertible = true;
+console.log(favCar);
+
