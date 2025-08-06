a35b0156d71e14190c9808225d80e1fb485b8f96
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-06 10:28:34 -0400

    Refactor fib function to improve readability and maintainability of base case handling

--- Module-03-Exercises/fibonacci-sequence.js
+++ Module-03-Exercises/fibonacci-sequence.js
@@ -1,8 +1,17 @@
 function fib(n) {
     // Step 1: Define the base case(s) here.
-    if (n === 0) return 0; // Base case: Fibonacci(0)
-    if (n === 1) return 1; // Base case: Fibonacci(1)
+    if (n === 0) 
+    {
+    return 0;
+    }
+
+    if (n === 1 || n === 2){
+        return 1;
+    }
     
     // Step 2: Define the recursive case here.
     return fib(n - 1) + fib(n - 2); // Recursive case
 }
+
+console.log(fib(10));
+
